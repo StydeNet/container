ac15cf9156d638e8410ecb223a636b34b292b2e0
Duilio Palacios <i@duilio.me>
2016-01-26 11:08:22 +0000

    First commit

---
+++ src/ContainerException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Styde\Container;
+
+use Exception;
+
+class ContainerException extends Exception
+{
+
+}
---
+++ src/Provider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Styde\Container;
+
+abstract class Provider
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    abstract public function register();
+
+}
